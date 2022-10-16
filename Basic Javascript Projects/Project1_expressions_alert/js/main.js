78d64ae6bd3ab0a53c547d13324a0c8430ee369a
GerardZaragoza <gerard.zaragoza1983@gmail.com>
2022-10-16 19:56:33 -0400

    update

--- Basic Javascript Projects/Project1_expressions_alert/js/main.js
+++ Basic Javascript Projects/Project1_expressions_alert/js/main.js
@@ -1,7 +1,17 @@
 window.alert("Hello World")
+var A = "string assignment" //this creates a pop window that says "Hello World"
+window.alert(A) // this displays the message written in the variable A
 
-var Family = "The Zaragoza's", Dad = "Gerard" , Mom = "Kathlene", FirstSon = "Gerard Xamuel", SecondSon = "Gerard Luka", FirstDaughter = "Kathlene Xiane", SecondDaughter = "Kathlene Xamantha";
-document.write (SecondSon);
-document.write ("&nbsp")
-document.write ("<br />")
-document.write (3+3);
+
+var b = "Hello" + "world"; // concatenated 
+document.write (b); // this displays the message written in the variable b
+
+document.write ("<br />") // this creates a page break in the script
+
+var Family = "The Zaragoza's", Dad = "Gerard" , Mom = "Kathlene", FirstSon = "Gerard Xamuel",
+SecondSon = "Gerard Luka", FirstDaughter = "Kathlene Xiane", SecondDaughter = "Kathlene Xamantha";// this are mutiple multiple variables
+document.write (SecondSon); //this displays the chosen variable among the multiple variables indicated in above
+
+document.write ("<br />")// page break
+
+document.write (3+3); // mathematical expression
