c416803a7f03f75f618276cab91ef138247935ae
GerardZaragoza <gerard.zaragoza1983@gmail.com>
2022-10-19 11:34:19 -0400

    update

--- Basic Javascript Projects/project2_functions/js/main.js
+++ Basic Javascript Projects/project2_functions/js/main.js
@@ -1,8 +1,10 @@
-function displaydate ()  { 
-    var str = "this is the button text";
-document.getElementById("Date") .innerHTML = Date ();
+function displaydate ()  { // this is the function
+    var str = "this is the button text"; // this is the name of the variable
+document.getElementById("Date") .innerHTML = Date ();// this is the function that calls the i.d. date
 }
-function math (p1, p2) {
-    return p1 * p2;
+
+function Myoperator() {
+        var sentence = "this is an "; //the variable
+        sentence += "operator assignment"; // this is the operator to concatenate the string 
+        document.getElementById("operator").innerHTML=sentence;// this is the method to display the variable
 }
-document.getElementById("math") .innerHTML = math(3, 5);
