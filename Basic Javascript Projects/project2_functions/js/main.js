c2eb05b59960a62a0629083c37d3107981f944df
GerardZaragoza <gerard.zaragoza1983@gmail.com>
2022-10-19 10:51:53 -0400

    update

---
+++ Basic Javascript Projects/project2_functions/js/main.js
@@ -0,0 +1,8 @@
+function displaydate ()  { 
+    var str = "this is the button text";
+document.getElementById("Date") .innerHTML = Date ();
+}
+function math (p1, p2) {
+    return p1 * p2;
+}
+document.getElementById("math") .innerHTML = math(3, 5);
