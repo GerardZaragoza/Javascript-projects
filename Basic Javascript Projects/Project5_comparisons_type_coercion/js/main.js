758db4e64f970fce4a04d3d620e7af14cdb71037
GerardZaragoza <gerard.zaragoza1983@gmail.com>
2022-10-29 14:36:03 -0400

    update

---
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/js/main.js
@@ -0,0 +1,16 @@
+document.write("10" + 5);
+document.write ("<br />")
+
+function my_Function () {
+    document.getElementById("test").innerHTML  = 0/0;
+}
+
+function my_function() {
+    document.getElementById("test2").innerHTML = isNaN ('string or no');
+}
+
+document.write ("<br />")
+
+function this_function() {
+    document.getElementById("test3").innerHTML = isNaN ('12453.657432423');
+}
